b865e61b77740add69fe0a9d09657a51fdce66dc
LZhuY <longzhuyi@gmail.com>
2018-09-03 10:57:05 +0800

    dy 376
    dy 376

--- coded.h
+++ coded.h
@@ -456,4 +456,19 @@ public:
 		}
 		return cnt == k;
 	}
+
+	int wiggleMaxLength376(int[] nums) {
+        int n = nums.length;
+        if(n <= 1)
+            return n;
+        int result = nums[0] != nums[1] ? 2:1;
+        int flag = nums[1] - nums[0];
+        for(int i=2;i<nums.length;i++){
+            if((nums[i] - nums[i-1]) * flag < 0 || (flag==0 && (nums[i] - nums[i-1]) != 0)){
+                result ++;
+                flag = nums[i] - nums[i-1];
+            }
+        }
+        return result;
+    }
 };

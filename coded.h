f7d434b7dd582d3e122ca5395244bb416b22fb11
LZhuY <longzhuyi@gmail.com>
2018-09-03 20:10:12 +0800

    best time to buy and sell
    best time to buy and sell

--- coded.h
+++ coded.h
@@ -533,4 +533,34 @@ public:
 		}
 		return sell;
 	}
+
+	//buy and sell k tiem
+	//我们定义local[i][j]为在到达第i天时最多可进行j次交易并且最后一次交易在最后一天卖出的最大利润，此为局部最优。
+	//然后我们定义global[i][j]为在到达第i天时最多可进行j次交易的最大利润，此为全局最优。它们的递推式为：
+
+	//local[i][j] = max(global[i - 1][j - 1] + max(diff, 0), local[i - 1][j] + diff)
+	//global[i][j] = max(local[i][j], global[i - 1][j])，
+	int maxProfit(int k, vector<int> &prices) {
+        if (prices.empty()) return 0;
+        if (k >= prices.size()) return solveMaxProfit(prices);
+        int g[k + 1] = {0};
+        int l[k + 1] = {0};
+        for (int i = 0; i < prices.size() - 1; ++i) {
+            int diff = prices[i + 1] - prices[i];
+            for (int j = k; j >= 1; --j) {
+                l[j] = max(g[j - 1] + max(diff, 0), l[j] + diff);
+                g[j] = max(g[j], l[j]);
+            }
+        }
+        return g[k];
+    }
+    int solveMaxProfit(vector<int> &prices) {
+        int res = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] - prices[i - 1] > 0) {
+                res += prices[i] - prices[i - 1];
+            }
+        }
+        return res;
+    }
 };
